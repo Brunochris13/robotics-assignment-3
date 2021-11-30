a22a00a0eb5697dd3eba61580fad2b350d6a8dd4
Christos <cefstathiou13@gmail.com>
2021-11-30 21:29:02 +0000

    Added provide_initial_pose_estimate.py and provide_goal_pose.py

---
+++ src/navigation/provide_initial_pose_estimate.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import PoseWithCovarianceStamped
+from tf.transformations import quaternion_from_euler
+
+def pub_init_pose():
+    rospy.init_node('init_pos')
+    pub = rospy.Publisher('/initialpose', PoseWithCovarianceStamped, queue_size = 10)
+    rospy.sleep(1)
+    checkpoint = PoseWithCovarianceStamped()
+
+    checkpoint.header.frame_id = "/map"
+
+    checkpoint.pose.pose.position.x = 0.0
+    checkpoint.pose.pose.position.y = 0.0
+    checkpoint.pose.pose.position.z = 0.0
+
+    [x,y,z,w]=quaternion_from_euler(0.0,0.0,0.0)
+    checkpoint.pose.pose.orientation.x = x
+    checkpoint.pose.pose.orientation.y = y
+    checkpoint.pose.pose.orientation.z = z
+    checkpoint.pose.pose.orientation.w = w
+
+    pub.publish(checkpoint)
+
+if __name__ == '__main__':
+    try:
+        pub_init_pose()
+    except rospy.ROSInterruptException:
+        pass
