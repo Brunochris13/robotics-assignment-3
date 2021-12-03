3b9ee129cc0389fcc9adc7c7d144c8157d6926cd
Wonseok Choi <ws4423@gmail.com>
2021-12-03 15:43:15 +0000

    vision.py

--- src/agent/parts/vision.py
+++ src/agent/parts/vision.py
@@ -1,3 +1,21 @@
+import os, random
+from interaction.face.detector import AgeGenderDetector
+
+
 class Vision():
     def __init__(self):
-        pass
+        self.scanner = AgeGenderDetector()
+        self.dir_path = "src/interaction/face/images"
+
+    def scan(self):
+        image = v.getImage
+        ppl = self.scanner.get_age_predictions(image)
+        return ppl
+
+    def getImage(self):
+        image = random.choice(os.listdir(self.dir_path))
+
+
+if __name__ == "__main__":
+    v = Vision()
+    v.scanner.display_img("test", v.getImage)
